e5fe7b6794ce88f4269e0770723b6b7b925d8fd9
Maxim Tischenko <mtishchenko@smedialink.com>
2016-10-13 12:26:38 +0300

    Initial Commit

---
+++ TestVIPERTests/TestVIPERTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestVIPERTests.swift
+//  TestVIPERTests
+//
+//  Created by Maxim Tischenko on 13.10.16.
+//  Copyright © 2016 Maxim Tischenko. All rights reserved.
+//
+
+import XCTest
+@testable import TestVIPER
+
+class TestVIPERTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
